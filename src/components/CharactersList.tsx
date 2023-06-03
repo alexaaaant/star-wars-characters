ee13d4e4c3c9d2f7e3dac84ecbf4b871708ee7ae
alexaaaant <alexaaaant@gmail.com>
2023-06-03 14:49:53 +0300

    display list of characters

--- src/components/CharactersList.tsx
+++ src/components/CharactersList.tsx
@@ -1,4 +1,35 @@
+import { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import { Character } from '../types/characters';
+
 export function CharactersList() {
-    return <div>List</div>;
-  }
-  
+  const [characters, setCharacters] = useState<Character[]>([]);
+
+  const fetchCharacters = async () => {
+    try {
+      const response = await fetch('https://swapi.dev/api/people');
+      const data = await response.json();
+      setCharacters(data.results);
+    } catch (error) {
+      console.log('Error fetching characters:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchCharacters();
+  },        []);
+
+  return (
+    <div>
+      <ul>
+        {characters.map((character) => (
+          <li key={character.name}>
+            <Link to={`/character/${character.name}`}>{character.name}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default CharactersList;
