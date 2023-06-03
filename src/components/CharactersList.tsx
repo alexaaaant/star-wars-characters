4ad94af9233556f5961b858b236a8c3847eac9d1
alexaaaant <alexaaaant@gmail.com>
2023-06-03 15:51:56 +0300

    implement infinity scrolling

--- src/components/CharactersList.tsx
+++ src/components/CharactersList.tsx
@@ -1,6 +1,6 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useCallback, useRef } from 'react';
 import { Link } from 'react-router-dom';
-import { Card, CardContent, Grid, TextField, Typography } from '@mui/material';
+import { Card, CardContent, CircularProgress, Grid, TextField, Typography } from '@mui/material';
 import { Character } from '../types/characters';
 import { useDebounce } from '../common/hooks/useDebounce';
 
@@ -8,19 +8,49 @@ export function CharactersList() {
   const [characters, setCharacters] = useState<Character[]>([]);
   const [searchQuery, setSearchQuery] = useState('');
   const debouncedSearchQuery = useDebounce(searchQuery);
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+  const [listCompeleted, setListCompleted] = useState(false);
+  const observerRef = useRef<IntersectionObserver | null>(null);
 
-  const fetchCharacters = async () => {
+  const loadCharacters = useCallback(async () => {
+    setIsLoading(true);
+
     try {
-      const response = await fetch('https://swapi.dev/api/people');
+      const response = await fetch(`https://swapi.dev/api/people?page=${page}`);
       const data = await response.json();
-      setCharacters(data.results);
+      if (!data.next) {
+        setListCompleted(true);
+      }
+
+      setCharacters((prevCharacters) => [...prevCharacters, ...data.results]);
     } catch (error) {
-      console.log('Error fetching characters:', error);
+      console.log('Error loading characters:', error);
     }
-  };
+
+    setIsLoading(false);
+  },                                 [page]);
 
   useEffect(() => {
-    fetchCharacters();
+    loadCharacters();
+  },        [loadCharacters]);
+
+  useEffect(() => {
+    observerRef.current = new IntersectionObserver((entries) => {
+      if (entries[0].isIntersecting) {
+        setPage((prevPage) => prevPage + 1);
+      }
+    });
+
+    if (observerRef.current) {
+      observerRef.current.observe(document.getElementById('observer')!);
+    }
+
+    return () => {
+      if (observerRef.current) {
+        observerRef.current.disconnect();
+      }
+    };
   },        []);
 
   const handleSearchChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -38,7 +68,7 @@ export function CharactersList() {
         value={searchQuery}
         onChange={handleSearchChange}
       />
-      <Grid container={true} spacing={2}>
+      <Grid container={true} spacing={2} justifyContent={'center'}>
         {filteredCharacters.map((character) => (
           <Grid item={true} xs={12} sm={6} md={4} key={character.name}>
             <Link to={`/character/${character.name}`} style={{ textDecoration: 'none' }}>
@@ -52,6 +82,8 @@ export function CharactersList() {
             </Link>
           </Grid>
         ))}
+        {!listCompeleted && <div id="observer" />}
+        {isLoading && <CircularProgress sx={{ m: 1 }} />}          
       </Grid>
     </div>
   );
